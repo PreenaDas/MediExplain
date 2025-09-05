31bed8272cce86048c64bd0ee1fa6ea5d2d0a5b6
PreenaDas <137705116+PreenaDas@users.noreply.github.com>
2025-09-05 16:29:10 +0000

    setup: frontend + tailwind + vite working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        🚀 MediExplain Frontend is Working!!!!yoohoooo
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
